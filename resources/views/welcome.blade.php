3982820173181e0204f0b2e5f24588e997f2010c
Matteo Pompei <datxizzan@gmail.com>
2022-03-16 23:09:13 +0100

    Cambiato percorso "home"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -72,7 +72,7 @@
     @if (Route::has('login'))
       <div class="top-right links">
         @auth
-          <a href="{{ url('/admin') }}">Home</a>
+          <a href="{{ route('admin.home') }}">Home</a>
         @else
           <a href="{{ route('login') }}">Login</a>
 
